dd4b6db72b59b3c8a159e6d1e659c115f78516d9
prasanth-277 <prasanthreddy549@gmail.com>
2021-06-10 16:28:45 +0530

    added email.js file

--- utils/email.js
+++ utils/email.js
@@ -2,6 +2,14 @@ const nodemailer = require("nodemailer");
 
 const sendEmail = (options) => {
   //create transporter
+  const transporter = nodemailer.createTransport({
+      service:'Gmail',
+      auth:{
+          user:'email@gmail.com',
+          pass:'123123123'
+      }
+      //Activate in gmail less secure app option
+  })
   //define email options
   //send the email
 };
